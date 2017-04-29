ef720704d8f906a450cf6da0fdd42f054d60d6fa
hammad tariq <hammadtariq65@gmail.com>
2017-04-30 00:08:57 +0500

    redundant code

--- routes/waypoints.js
+++ routes/waypoints.js
@@ -23,29 +23,6 @@ router.get('/', function (req, res, next) {
     res.send({ "status": true, wayPoints: response.json });
   });
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  var param = req.query;
-  client.setex("username", 60, "totalStars");
-  client.get(username, function(error, result) {
-        console.log("Redis Data ", result);
-  });
-  var obj = JSON.parse(fs.readFileSync('data/development.json', 'utf8'));
-  var googleMapsClient = require('@google/maps').createClient({
-    key: obj.key
-    });
-   var a =  JSON.parse(param.origin);
-   var b =  JSON.parse(param.destination);
-  
-var query = {
-  origin: a,
-  destination: b,
- }
-googleMapsClient.directions(query, (err, response) => {
-      if (err) console.log(err)
-       res.send({"status" : true, wayPoints : response.json});
-    })
-    
 });
 
 module.exports = router;
